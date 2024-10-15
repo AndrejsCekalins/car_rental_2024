a5389412f93dbd3d00fb460eb7444e8952fdde99
AndrejsCekalins <batarejka2006@gmail.com>
2024-10-15 10:55:23 +0300

    car_rental_2024_lombok_refactoring

--- car-rental-app/src/main/java/org/rental/rest/CarRentPriceCalculationResponse.java
+++ car-rental-app/src/main/java/org/rental/rest/CarRentPriceCalculationResponse.java
@@ -1,8 +1,17 @@
 package org.rental.rest;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CarRentPriceCalculationResponse {
 
 private String personFirstName;
@@ -10,48 +19,4 @@ private String personLastName;
 private Date agreementDateFrom;
 private Date getAgreementDateTo;
 
-private BigDecimal agreementPrice;
-
-    public CarRentPriceCalculationResponse() {
-    }
-
-    public String getPersonFirstName() {
-        return personFirstName;
-    }
-
-    public void setPersonFirstName(String personFirstName) {
-        this.personFirstName = personFirstName;
-    }
-
-    public String getPersonLastName() {
-        return personLastName;
-    }
-
-    public void setPersonLastName(String personLastName) {
-        this.personLastName = personLastName;
-    }
-
-    public Date getAgreementDateFrom() {
-        return agreementDateFrom;
-    }
-
-    public void setAgreementDateFrom(Date agreementDateFrom) {
-        this.agreementDateFrom = agreementDateFrom;
-    }
-
-    public Date getGetAgreementDateTo() {
-        return getAgreementDateTo;
-    }
-
-    public void setGetAgreementDateTo(Date getAgreementDateTo) {
-        this.getAgreementDateTo = getAgreementDateTo;
-    }
-
-    public BigDecimal getAgreementPrice() {
-        return agreementPrice;
-    }
-
-    public void setAgreementPrice(BigDecimal agreementPrice) {
-        this.agreementPrice = agreementPrice;
-    }
 }
