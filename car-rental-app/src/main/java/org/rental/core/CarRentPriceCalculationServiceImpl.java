05ff150c758a86866980e0c80563a47da12d0a9f
AndrejsCekalins <batarejka2006@gmail.com>
2024-10-15 10:55:23 +0300

    car_rental_2024_calculate_days_between

--- car-rental-app/src/main/java/org/rental/core/CarRentPriceCalculationServiceImpl.java
+++ car-rental-app/src/main/java/org/rental/core/CarRentPriceCalculationServiceImpl.java
@@ -2,8 +2,17 @@ package org.rental.core;
 
 import org.rental.rest.CarRentPriceCalculationRequest;
 import org.rental.rest.CarRentPriceCalculationResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
+@Component
 public class CarRentPriceCalculationServiceImpl implements CarRentPriceCalculationService {
+
+    @Autowired
+    private DateTimeService dateTimeService;
+
     @Override
     public CarRentPriceCalculationResponse calculatePrice(CarRentPriceCalculationRequest request) {
         CarRentPriceCalculationResponse response = new CarRentPriceCalculationResponse();
@@ -11,6 +20,10 @@ public class CarRentPriceCalculationServiceImpl implements CarRentPriceCalculati
         response.setPersonLastName(request.getPersonLastName());
         response.setAgreementDateFrom(request.getAgreementDateFrom());
         response.setGetAgreementDateTo(request.getAgreementDateTo());
+
+        var daysBetween = dateTimeService.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo());
+        response.setAgreementPrice(new BigDecimal(daysBetween));
+
         return response;
     }
 }
